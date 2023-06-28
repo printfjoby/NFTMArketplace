9b7e05f00422afe155cbe433143fbbc989526a68
Joby Augustine <printfjoby@gmail.com>
2023-06-28 21:46:09 +0530

    Added networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -12,6 +17,12 @@ module.exports = {
     },
     paths: {
       artifacts: "./artifacts",
+    },
+    networks: {
+      sepolia: {
+        url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+        accounts: [SEPOLIA_PRIVATE_KEY]
+      }
     },
     external: {
       contracts: [
